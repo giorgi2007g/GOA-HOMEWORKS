1d52e229ef73c71ecaf5fe3c74062c563dd08be3
giorgi2007g <bibilashvilig13@gmail.com>
2024-03-04 09:32:24 -0800

    five

---
+++ Day 5/lesson5.py
@@ -0,0 +1,8 @@
+your_age = int(input("sheni asaki?: "))
+parent_age = int(input("mshoblis asaki?: "))
+
+comperation = your_age < parent_age 
+
+print(comperation)
+#13 > 45 = false 
+print(type(comperation))
